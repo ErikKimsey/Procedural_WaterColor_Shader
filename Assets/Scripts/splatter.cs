2211a605e12d5159d186109690862bf29f0be80a
Erik Kimsey <kimseyerik@gmail.com>
2020-09-09 10:21:28 -0400

    First

---
+++ Assets/Scripts/splatter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class splatter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxScale, minScale;
+    public float minAspectRatio, maxAspectRatio;
+    public Sprite src;
+    public Texture dst;
+
+    void Start()
+    {
+
+    }
+
+    void BlitDrop(Sprite src, Texture dst)
+    {
+	// Select a random scale for this drop
+	float scaleX = minScale + 
+		(Random.value * (maxScale - minScale));
+
+	float aspectRatio = minAspectRatio + 
+		(Random.value * (maxAspectRatio - minAspectRatio));
+
+	float scaleY = scaleX * aspectRatio;
+
+	// Calculate the destination rect for this drop
+	Rect dstRect = new Rect()
+	{
+		width = src.textureRect.width * scaleX,
+		height = src.textureRect.height * scaleY
+	};
+
+	dstRect.x = random.Next(0, dst.width - (int) dstRect.width);
+	dstRect.y = random.Next(0, dst.width - (int) dstRect.height);
+
+	// Copy pixels from the drop (src) to the stain (dst)
+	for (int y = 0; y < dstRect.height; ++y)
+	{
+		for (int x = 0; x < dstRect.width; ++x)
+		{
+			// Since the source drop is scaled, we bilinearly sample the 4 pixels 
+			// around the exact location we want to sample
+			Color srcColor = SampleBilinear(src.texture, 
+				srcRect.x + x / scaleX, srcRect.y + y / scaleY);
+
+			Color dstColor = dst.GetPixel((int)dstRect.x + x, (int) dstRect.y + y);
+			
+			// Write the new pixel value to the stain texture by taking
+			// the maximum of the previous value and the new value
+			dstColor = new Color(0, 0, 0, Math.Max(srcColor.a, dstColor.a));
+
+			dst.SetPixel((int)dstRect.x + x, (int)dstRect.y + y, dstColor);
+		}
+	}
+}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Shaders/splatter.shader
@@ -0,0 +1,59 @@
+﻿Shader "Unlit/splatter"
+{
+Properties {
+        _Color ("Main Color", Color) = (1,1,1,0.5)
+        _MainTex ("Texture", 2D) = "white" { }
+    }
+    SubShader {
+        Pass {
+
+        CGPROGRAM
+        #pragma vertex vert
+        #pragma fragment frag
+
+        #include "UnityCG.cginc"
+
+        
+        float _Smoothing;
+        sampler2D _MainTex;
+        fixed4 _Color;
+        float4 _MainTex_ST;
+
+        struct v2f {
+            float4 pos : SV_POSITION;
+            float2 uv : TEXCOORD0;
+            fixed4 color: _Color;
+        };
+
+        v2f vert (appdata_base v)
+        {
+            v2f o;
+            o.pos = UnityObjectToClipPos(v.vertex);
+            o.uv = TRANSFORM_TEX (v.texcoord, _MainTex);
+            return o;
+        }
+
+        fixed4 frag (v2f i) : SV_Target
+        {
+            fixed4 texcol = tex2D (_MainTex, i.uv);
+            return texcol * _Color;
+            // Sample the distance value from the texture
+            // A value of 0.5 means that the pixel is exactly on the shape edge
+            fixed4 sdf = tex2D(_MainTex, i.uv);
+
+            // Use our smoothing value to determine an alpha value for this pixel 
+            fixed alpha = smoothstep(0.5 - _Smoothing, 0.5 + _Smoothing, sdf.a);
+
+            // Combine the alpha with the vertex color to calculate the pixel color
+            fixed4 c;
+            c.rgb = i.color.rgb;
+            c.a = i.color.a * alpha;
+
+            return c;
+        }
+        ENDCG
+
+        }
+    }
+}
+
